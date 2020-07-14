774929cc4aefbb0d8393b489bf11c4ea48f4150c
Agustin Alvarez <agustinalvarez@agustins-mbp.home>
2020-07-14 16:01:28 -0700

    data ready, woeking on maps

--- client/src/components/countries.js
+++ client/src/components/countries.js
@@ -5,29 +5,40 @@ import gql from 'graphql-tag';
 
 
 //Component Query
-const COUNTRIES_QUERY = gql`
-query countriesQuery {
-  countries {
-    todayCases
-  }
-}
 
-`;
 
 //Component
 function Countries() {
 
+
+
+    const COUNTRIES_QUERY = gql`
+    query countriesQuery {
+      countries {
+        country
+        todayCases
+      }
+    }
+
+    `;
     const { loading, error, data } = useQuery(COUNTRIES_QUERY, { errorPolicy: 'all' });
 
-  if (loading) return 'Loading...';
-  // if (error) console.log(error); return `Error! ${error}`;
-  if (data) console.log(data);
-    return(
-      <div>
-        <h1>Countries</h1>
-        <h1>{data.countries[0].todayCases}</h1>
-      </div>
-    )
+    if (loading) return 'Loading...';
+    // if (error) console.log(error); return `Error! ${error}`;
+    if(data) {
+      return(
+        <div>
+          <h1>Countries</h1>
+          <h1>{data.countries[0].country}</h1>
+
+
+        </div>
+      )
+
+    }
+
+
+
 
 }
 
