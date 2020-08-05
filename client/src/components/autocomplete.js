7c88ff5366de1a1e20c11ae6de372e721d567e0f
Agustin Alvarez <agustinalvarez@agustins-mbp.home>
2020-08-05 16:18:46 -0700

    autocomplete works

--- client/src/components/autocomplete.js
+++ client/src/components/autocomplete.js
@@ -3,28 +3,44 @@ import React, { useState, useEffect } from 'react';
 
 function AutoComplete({ historical, getCountry}){
   const [options, setOptions] = useState([])
-console.log(historical);
+
 
 const countriesNames = []
 
  for(let country of historical){
-    countriesNames.push(country.country);
+   let countryName = country.country.toLowerCase()
+    countriesNames.push(countryName);
   }
 
 function getOptions(event){
-  const entry = event.target.value
+  const entry = event.target.value.toLowerCase()
   const list = []
   for(let name of countriesNames){
     if(name.includes(entry)){
       list.push(name)
     }
-  } console.log(list);
+  }
+  const completeOptions = []
+  if(list.length <= 7){
+    for(let countryName of list){
+      let array = countryName.split('')
+      let first = array[0].toUpperCase()
+       array.shift()
+      let fileName = first.concat(array).replace(/,/g, '')
+      completeOptions.push(fileName)
+    }
+  } setOptions(completeOptions);
+
 }
 
 
   return(
     <>
       <input onChange={getOptions} type='text' placeholder='Country'/><br/>
+      <ul> {options.map((i, key) => {
+        return <li key={key}>{options[i]}</li>
+      })}
+      </ul>
     </>
   )
 }
